ea61def0ce3eb6a51d2f173385c974c504a22f96
Rockython <61116928+Rockython@users.noreply.github.com>
2022-12-10 21:25:40 +0700

    Recommit

--- src/js/script.js
+++ src/js/script.js
@@ -11,3 +11,41 @@ page_buttons.forEach(element => {
     })
 
 });
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    function openMenu(numPage = 1) {
+        function setPagHtmlVals (numPage, sizeOfPag) {
+            let backPag = document.getElementsByClassName("backPag")[0];
+            let ftPag = document.getElementsByClassName("ftPag")[0];
+            let scPag = document.getElementsByClassName("scPag")[0];
+            let tdPag = document.getElementsByClassName("tdPag")[0];
+            let frontPag = document.getElementsByClassName("frontPag")[0];
+            if (numPage == 1) {
+                backPag.disabled = true;
+            } else if (numPage == sizeOfPag - 1) {
+                console.log("numPage == sizeOfPag - 1");
+            }
+        }
+
+        var x = new XMLHttpRequest();
+        const linkToDish = "https://food-delivery.kreosoft.ru/api/dish";
+        x.open("GET", `${linkToDish}?page=${numPage}`, true);
+        x.onload = function () {
+            const menuPage = JSON.parse(x.responseText);
+            console.log(menuPage.dishes)
+            console.log(menuPage.pagination)
+        }
+        x.send(null);
+        // задаём значения для innerhtml тутъ
+        //setPagHtmlVals(numPage);
+        // конецтут
+        return numPage;
+    }
+
+    let numPage = openMenu();
+    document.querySelector(".menuLink").addEventListener("click", () => {
+        numPage = openMenu();
+    });
+});
