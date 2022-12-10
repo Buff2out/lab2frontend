4f73b37ef12d3629366873d50ab030036a06875d
Rockython <61116928+Rockython@users.noreply.github.com>
2022-12-10 22:01:52 +0700

    commitBUFF

--- src/js/script.js
+++ src/js/script.js
@@ -1,34 +1,5 @@
-var page_buttons = document.querySelectorAll(".page-link");
-
-var a = page_buttons[1];
-
-page_buttons.forEach(element => {
-
-    element.addEventListener("click", () => {
-        a.classList.remove("active");
-        a = element;
-        a.classList.add("active");
-    })
-
-});
-
-
-
 document.addEventListener('DOMContentLoaded', () => {
     function openMenu(numPage = 1) {
-        function setPagHtmlVals (numPage, sizeOfPag) {
-            let backPag = document.getElementsByClassName("backPag")[0];
-            let ftPag = document.getElementsByClassName("ftPag")[0];
-            let scPag = document.getElementsByClassName("scPag")[0];
-            let tdPag = document.getElementsByClassName("tdPag")[0];
-            let frontPag = document.getElementsByClassName("frontPag")[0];
-            if (numPage == 1) {
-                backPag.disabled = true;
-            } else if (numPage == sizeOfPag - 1) {
-                console.log("numPage == sizeOfPag - 1");
-            }
-        }
-
         var x = new XMLHttpRequest();
         const linkToDish = "https://food-delivery.kreosoft.ru/api/dish";
         x.open("GET", `${linkToDish}?page=${numPage}`, true);
@@ -43,8 +14,38 @@ document.addEventListener('DOMContentLoaded', () => {
         // конецтут
         return numPage;
     }
-
     let numPage = openMenu();
+    var page_buttons = document.querySelectorAll(".page-link");
+    var a = page_buttons[numPage];
+    page_buttons.forEach(element => {
+        element.addEventListener("click", () => {
+            a.classList.remove("active");
+            a = element;
+            a.classList.add("active");
+            // случаи:
+            // 1 !2! 3 click << (станет !1! 2 3)
+            // 1 2 !3! click >> (станет: 2 3 !4!)
+            // 3 !4! 5 click >> (станет 3 4 !5!) нужно учесть size
+            if ("«" === a.textContent) {
+                numPage--;
+                openMenu(numPage);
+
+            } else if ("»" === a.textContent) {
+                if (a.) {
+                    numPage++;
+                    page_buttons[1].textContent = String(Number(page_buttons[1].textContent) + 1);
+                    page_buttons[2].textContent = String(Number(page_buttons[2].textContent) + 1);
+                    page_buttons[3].textContent = String(Number(page_buttons[3].textContent) + 1);
+                    openMenu(numPage);
+                } else {
+
+                }
+            }
+            else {
+                numPage = openMenu(Number(a.textContent));
+            }
+        })
+    });
     document.querySelector(".menuLink").addEventListener("click", () => {
         numPage = openMenu();
     });
