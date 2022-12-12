2145e48a765c30a577b0b08767642755b65d9530
Rockython <61116928+Rockython@users.noreply.github.com>
2022-12-12 19:51:02 +0700

    bugfixes with pagbuttons

--- src/js/script.js
+++ src/js/script.js
@@ -11,7 +11,7 @@ var menuPage = 0;
 
 document.addEventListener('DOMContentLoaded', () => {
     function openMenu(numPage = 1) {
-        history.pushState(null, null, `?page=${numPage}`);
+        //history.pushState(null, null, `?page=${numPage}`);
         function setValsToCards() {
             imgs = document.querySelectorAll(".card-img-top");
             cTitles = document.querySelectorAll(".card-title");
@@ -40,70 +40,75 @@ document.addEventListener('DOMContentLoaded', () => {
         // конецтут
         return numPage;
     }
-    function releasePagBtn (page_buttons, i, prev) {
-        page_buttons[prev].classList.remove("active");
-        page_buttons[i].classList.add("active");
+    function releasePagBtn (i, paginObj) {
+        paginObj.page_buttons[paginObj.prev].classList.remove("active");
+        paginObj.page_buttons[i].classList.add("active");
         // случаи:
         // 1 !2! 3 click << (станет !1! 2 3)
         // 1 2 !3! click >> (станет: 2 3 !4!)
         // 3 !4! 5 click >> (станет 3 4 !5!) нужно учесть size
-        if ("«" === page_buttons[i].textContent) {
-            page_buttons[i].classList.remove("active");
-            if (prev === 1) {
-                page_buttons[1].textContent = String(Number(page_buttons[1].textContent) - 1);
-                page_buttons[2].textContent = String(Number(page_buttons[2].textContent) - 1);
-                page_buttons[3].textContent = String(Number(page_buttons[3].textContent) - 1);
+        if ("«" === paginObj.page_buttons[i].textContent) {
+            paginObj.page_buttons[i].classList.remove("active");
+            if (paginObj.prev === 1) {
+                paginObj.page_buttons[1].textContent = String(Number(paginObj.page_buttons[1].textContent) - 1);
+                paginObj.page_buttons[2].textContent = String(Number(paginObj.page_buttons[2].textContent) - 1);
+                paginObj.page_buttons[3].textContent = String(Number(paginObj.page_buttons[3].textContent) - 1);
 
-                page_buttons[prev].classList.add("active");
+                paginObj.page_buttons[paginObj.prev].classList.add("active");
                 // prev = prev;
             } else {
-                page_buttons[prev - 1].classList.add("active");
-                prev--;
+                paginObj.page_buttons[paginObj.prev - 1].classList.add("active");
+                paginObj.prev--;
             }
-            numPage--;
-            openMenu(numPage);
+            paginObj.numPage--;
+            openMenu(paginObj.numPage);
 
-        } else if ("»" === page_buttons[i].textContent) {
-            page_buttons[i].classList.remove("active");
-            if (prev === 3) {
-                page_buttons[1].textContent = String(Number(page_buttons[1].textContent) + 1);
-                page_buttons[2].textContent = String(Number(page_buttons[2].textContent) + 1);
-                page_buttons[3].textContent = String(Number(page_buttons[3].textContent) + 1);
+        } else if ("»" === paginObj.page_buttons[i].textContent) {
+            paginObj.page_buttons[i].classList.remove("active");
+            if (paginObj.prev === 3) {
+                paginObj.page_buttons[1].textContent = String(Number(paginObj.page_buttons[1].textContent) + 1);
+                paginObj.page_buttons[2].textContent = String(Number(paginObj.page_buttons[2].textContent) + 1);
+                paginObj.page_buttons[3].textContent = String(Number(paginObj.page_buttons[3].textContent) + 1);
 
-                page_buttons[prev].classList.add("active");
+                paginObj.page_buttons[paginObj.prev].classList.add("active");
                 // prev = prev;
             } else {
-                page_buttons[prev + 1].classList.add("active");
-                prev++;
+                paginObj.page_buttons[paginObj.prev + 1].classList.add("active");
+                paginObj.prev++;
             }
-            numPage++;
-            openMenu(numPage);
+            paginObj.numPage++;
+            openMenu(paginObj.numPage);
         } else {
-            numPage = openMenu(Number(page_buttons[i].textContent));
-            prev = i;
+            paginObj.numPage = openMenu(Number(paginObj.page_buttons[i].textContent));
+            paginObj.prev = i;
         }
-        if (1 === Number(page_buttons[1].textContent) && 1 === Number(page_buttons[prev].textContent)) {
-            page_buttons[0].classList.add("disabled");
-        } else if (Number(menuPage.pagination.count) === Number(page_buttons[prev].textContent) && Number(menuPage.pagination.count) === Number(page_buttons[3].textContent)) {
-            page_buttons[4].classList.add("disabled");
+        if (1 === Number(paginObj.page_buttons[1].textContent) && 1 === Number(paginObj.page_buttons[paginObj.prev].textContent)) {
+            paginObj.page_buttons[0].classList.add("disabled");
+        } else if (Number(menuPage.pagination.count) === Number(paginObj.page_buttons[paginObj.prev].textContent) && Number(menuPage.pagination.count) === Number(paginObj.page_buttons[3].textContent)) {
+            paginObj.page_buttons[4].classList.add("disabled");
         } else {
-            page_buttons[0].classList.remove("disabled");
-            page_buttons[4].classList.remove("disabled");
+            paginObj.page_buttons[0].classList.remove("disabled");
+            paginObj.page_buttons[4].classList.remove("disabled");
         }
+        return paginObj;
     }
-    var GETObj = parseGetParams();
-    if (GETObj.page != null) {
-        let numPage = openMenu(Number(GETObj.page));
+    var GETParamsObj = parseGetParams();
+    var numPage = 1
+    if (GETParamsObj.page != null) {
+        numPage = openMenu(Number(GETParamsObj.page));
     } else {
-        let numPage = openMenu();
+        numPage = openMenu();
     }
-    var page_buttons = document.querySelectorAll(".page-link");
-    var curr = page_buttons[numPage];
-    var prev = 1;
-    for (let i = 0; i < page_buttons.length; i++) {
+    var paginObj = {
+        page_buttons: document.querySelectorAll(".page-link"),
+        prev: 1,
+        numPage: numPage
+    };
 
-        page_buttons[i].addEventListener("click", () => {
-            releasePagBtn(page_buttons, i, prev);
+    for (let i = 0; i < paginObj.page_buttons.length; i++) {
+
+        paginObj.page_buttons[i].addEventListener("click", () => {
+            paginObj = releasePagBtn(i, paginObj);
         });
     }
     document.querySelector(".menuLink").addEventListener("click", () => {
