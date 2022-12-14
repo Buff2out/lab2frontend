345774fa12c42b0d3951191cc8cac20737266e50
AdamAvocado <ravenweiss@outlook.com>
2022-12-15 03:52:20 +0700

    making standalone meal-id-card

--- item/src/item.js
+++ item/src/item.js
@@ -1,26 +1,55 @@
+// парсим получившуюся часть url (id блюда)
+function parseGetParams() {
+    var query = location.search.substr(1);
+    if (query === "") {
+        return "";
+    } else {
+        console.log("query");
+        console.log(query);
+        var params = query.split("&");
+        console.log("params");
+        console.log(params);
+        var result = {};
+        for (var i = 0; i < params.length; i++) {
+            var item = params[i].split("=");
 
+            const key = item[0].replace(/\[|\]/g, '')
+            const value = item[1].toLowerCase();
 
+            if (!result[key]) result[key] = [value]
+            else result[key].push(value)
 
+        }
+        console.log("result");
+        console.log(result);
+        return result.id[0];
+    }
+}
 
-let result = new Promise((resolve) => {
-    const pizdec = JSON.parse(testFunc());
-    console.log(pizdec)
+// Обращаемся к серверу (где query - id блюда)
+async function getMealId(mealId) {
 
-});
-
-console.log(testFunc)
-
-
-async function testFunc() {
-
-    const linkToDishId = "https://food-delivery.kreosoft.ru/api/dish/";
-    const response = await fetch(`${linkToDishId}`, {
+    const linkToDish = "https://food-delivery.kreosoft.ru/api/dish/";
+    const response = await fetch(`${linkToDish}${mealId}`, {
         method: 'GET',
         headers: {
             'Accept': 'application/json'
         }
-
     });
-
     return await response.json();
+
 }
+
+
+var mealId = parseGetParams();
+console.log(mealId);
+
+
+
+let getMealIdPromise = new Promise(function (resolve) {
+    resolve(getMealId(mealId));
+});
+
+getMealIdPromise.then(function (value) {
+    console.log(value);
+});
--- temp.js
+++
@@ -1,7 +0,0 @@
-// setting and removing an attribute to html-tag
-
-
-var a = document.querySelector("#wok-checkbox");
-a.setAttribute("checked", "")
-        OR
-a.removeAttribute("checked");
